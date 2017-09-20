5a8eacd313ae1d60ec8f0fc9ee4baa51f0356895
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-09-20 22:56:25 +0200

    put prototype outside function

--- scripts.js
+++ scripts.js
@@ -4,12 +4,13 @@ function Phone(brand, price, color, ram, internalStorage) {
 	this.color = color;
 	this.ram = ram;
 	this.internalStorage = internalStorage;
+}
 
-	Phone.prototype.printInfo = function() {
+
+Phone.prototype.printInfo = function() {
 		console.log("The phone is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". The RAM is " + this.ram + " GB" +
 					" and internal storage is " + this.internalStorage + " GB.");
 	}
-}
 
 var SamsungGalaxyS6 = new Phone("Samsung", 1200, "black", 3, 64);
 var iPhone6 = new Phone("Apple", 2000, "grey", 4, 64);
